2475dcffdcac757ecf9b0f856b0b4b349d39dd37
Dawid <Dawid_pacanowski@wp.pl>
2021-02-21 00:53:38 +0100

    update

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 app.get("/data", (req,res) => {
     res.send(importData);
-});
+})
+app.listen(port, () => {
+    console.log("test");
+})
