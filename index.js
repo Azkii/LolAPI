823ace6c72fd5a5f4b494c4d8399b7d755351de1
Dawid <Dawid_pacanowski@wp.pl>
2021-02-21 01:44:47 +0100

    static files + website added :>

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
-const website = require("./website/index.html");
+//website
+const path = require('path');
+const router = express.Router();
+
 let port = process.env.PORT || 3000;
 app.get("/data", (req,res) => {
     res.send(importData);
 })
-app.get("/",(req,res) => {
-    res.send(website);
-})
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.use("/websiteStatic", express.static('./websiteStatic'));
+
+app.use('/', router);
 app.listen(port, () => {
     console.log("test");
 })
