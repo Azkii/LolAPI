160edd1ddeb9f26ef96afca2e539d9864022c4ac
Dawid <Dawid_pacanowski@wp.pl>
2021-02-21 01:53:42 +0100

    fetch test

--- websiteStatic/app.js
+++ websiteStatic/app.js
@@ -1,1 +1,8 @@
 console.log("xdd");
+fetch(`https://lolchallangerapi.herokuapp.com/data`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        console.log(data);
+    });
