546e613c7227f40eee144cb5ec30f271124b6d14
Dawid <Dawid_pacanowski@wp.pl>
2021-02-21 01:01:51 +0100

    websiteTest

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
+const website = require("./website/index.html");
 let port = process.env.PORT || 3000;
 app.get("/data", (req,res) => {
     res.send(importData);
+})
+app.get("/",(req,res) => {
+    res.send(website);
 })
 app.listen(port, () => {
     console.log("test");
